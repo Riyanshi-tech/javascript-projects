d82db8f6700b6d39bc580f46d9f8f4be6829d085
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-09-05 08:42:54 +0530

    changing js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ function toggleAccordion(id) {
   const content = document.getElementById(`content-${id}`);
   const icon = document.getElementById(`icon-${id}`);
 
+  
   if (content.classList.contains("hidden")) {
+    
     content.classList.remove("hidden");
     icon.textContent = "-";
-  } else {
+}
+  else {
     content.classList.add("hidden");
     icon.textContent = "+";
+    
   }
 }
